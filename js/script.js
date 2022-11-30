f3ea9f1ea29d968949695c7285b95dd18a6ef94f
unknown <js.abramczyk@gmail.com>
2022-11-30 19:45:35 +0100

    error function added

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ const btn = document.querySelector('.btn');
 const URL = 'https://api.adviceslip.com/advice';
 
 const generateAdvice = () => {
-	axios.get(URL).then((res) => {
-		const adviceText = res.data.slip.advice;
-		advice.innerHTML = `"${adviceText}"`;
+	axios
+		.get(URL)
+		.then((res) => {
+			const adviceText = res.data.slip.advice;
+			advice.innerHTML = `"${adviceText}"`;
 
-		const id = res.data.slip.id;
-		adviceID.innerHTML = id;
-	});
+			const id = res.data.slip.id;
+			adviceID.innerHTML = id;
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
 };
 
 btn.addEventListener('click', generateAdvice);
